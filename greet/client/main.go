986acb9553027dad53f0c50d28326223e5f7ab10
awmtea <awmtea105.infinity@gmail.com>
2024-07-29 16:06:15 +0700

    server streaming

--- greet/client/main.go
+++ greet/client/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -9,7 +12,7 @@ import (
 	pb "github.com/khiangte85/grpc-go/greet/proto"
 )
 
-var addr string = "localhost:50051"
+var addr string = "localhost:50052"
 
 func main() {
 	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -23,4 +26,42 @@ func main() {
 	c := pb.NewGreetServiceClient(conn)
 
 	doGreet(c)
+	doGreetManyTimes(c)
+}
+
+
+func doGreet(c pb.GreetServiceClient) {
+	res, err := c.Greet(context.Background(), &pb.GreetRequest{
+		FirstName: "Khiangte",
+	})
+
+	if err != nil {
+		log.Fatalf("Greet call failed: %v\n", err)
+	}
+
+	log.Println(res)
+}
+
+func doGreetManyTimes(c pb.GreetServiceClient){
+	stream, err := c.GreetManyTimes(context.Background(), &pb.GreetRequest{
+		FirstName: "Lalmuanawma",
+	})
+
+	if err != nil {
+		log.Fatalf("rpc call error: %v\n", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("stream recv error: %v\n", err)
+		}
+
+		fmt.Println(msg)
+	}
 }
--- greet/server/main.go
+++ greet/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
 
@@ -8,7 +10,7 @@ import (
 	"google.golang.org/grpc"
 )
 
-var addr string = "0.0.0.0:50051"
+var addr string = "0.0.0.0:50052"
 
 type Server struct {
 	pb.GreetServiceServer
@@ -29,5 +31,28 @@ func main() {
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v\n", err)
 	}
+}
 
+func (s *Server) Greet(ctx context.Context, reg *pb.GreetRequest) (*pb.GreetResponse, error) {
+	log.Printf("Greet invoked with: %v\n", reg)
+
+	return &pb.GreetResponse{
+		Result: "Hello " + reg.FirstName,
+	}, nil
+}
+
+func (s *Server) GreetManyTimes(in *pb.GreetRequest, stream pb.GreetService_GreetManyTimesServer) error {
+	log.Printf("GreetManyTimes invoked with: %v\n", in.FirstName)
+
+	for i := 0; i < 10; i++ {
+		res := &pb.GreetResponse{
+			Result: fmt.Sprintf("Hello %s, number %d", in.FirstName, i),
+		}
+
+		if err := stream.Send(res); err != nil {
+			return err
+		} 
+	}
+
+	return nil
 }
